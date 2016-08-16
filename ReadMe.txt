aedaa59ba22581b08f2cdbace44dc7ae49a387a5
Sandi Yen <sandi.yen@gmail.com>
2016-08-16 15:52:46 -0400

    added argument to specify samples; added ability to specify multiple taxon filters; added default for src.folder to be current working directory

--- ReadMe.txt
+++ ReadMe.txt
@@ -12,11 +12,22 @@ Please read below for argument details------------------------------------------
 function to create a file that is compatible for oligotyping by Meren
 using the output files of mothur
 
-input: src.folder = string; path to where mothur output files are stored
-        taxon = string; concatinate with c() if specifying more than one; 
+input: src.folder = string; default is current working directory;
+                     path to where mothur output files are stored
+        taxon = vector of strings or list of string vectors; default NULL;
+                concatinate with c() if specifying more than one; 
                  sets the taxon you want to select; 
-                 example: c('Bacteria','Firmicutes')
-        output = string; default NULL (does not produce fasta file); 
-                 name of output file; .fasta extension required
+                 example: c('Bacteria','Firmicutes');
+                if want to process multiple taxonomic classes 
+                (and return multiple fasta files), provide taxon as list of vectors
+                example: list(c('Bacteria','Phylum1','Class1','Order1'),
+                              c('Bacteria','Phylum1','Class1','Order2'))
+        sample_name = vector of strings; default NULL;
+                     name of samples to be included in the fasta file
+        output = string or vector of strings; 
+                 default NULL (does not produce fasta file); 
+                 name of output file; .fasta extension required;
+                 when multiple taxon filters specified, 
+                 need to provide file name for each one 
 
-output: fasta file formated to oligotyping program requirements
+# output: fasta file(s) formated to oligotyping program requirements
--- oligotype_input.R
+++ oligotype_input.R
@@ -2,16 +2,28 @@
 # function to create a file that is compatible for oligotyping by Meren
 # using the output files of mothur
 
-# input: src.folder = string; path to where mothur output files are stored
-#        taxon = string; concatinate with c() if specifying more than one; 
+# input: src.folder = string; default is current working directory;
+#                     path to where mothur output files are stored
+#        taxon = vector of strings or list of string vectors; default NULL;
+#                concatinate with c() if specifying more than one; 
 #                 sets the taxon you want to select; 
-#                 example: c('Bacteria','Firmicutes')
-#        output = string; default NULL (does not produce fasta file); 
-#                 name of output file; .fasta extension required
+#                 example: c('Bacteria','Firmicutes');
+#                if want to process multiple taxonomic classes 
+#                (and return multiple fasta files), provide taxon as list of vectors
+#                example: list(c('Bacteria','Phylum1','Class1','Order1'),
+#                              c('Bacteria','Phylum1','Class1','Order2'))
+#        sample_name = vector of strings; default NULL;
+#                     name of samples to be included in the fasta file
+#        output = string or vector of strings; 
+#                 default NULL (does not produce fasta file); 
+#                 name of output file; .fasta extension required;
+#                 when multiple taxon filters specified, 
+#                 need to provide file name for each one 
 
-# output: fasta file formated to oligotyping program requirements
+# output: fasta file(s) formated to oligotyping program requirements
 
-oligotype_input <- function(src.folder, taxon=NULL, output=NULL){
+oligotype_input <- function(src.folder=getwd(), taxon=NULL, sample_name=NULL, 
+                            output=NULL){
   
   # function requirements------------------------------------------------------------
   #checking for required packages; installing those not yet installed
@@ -47,7 +59,15 @@ oligotype_input <- function(src.folder, taxon=NULL, output=NULL){
   count_df <- count
   colnames(count_df)[1] <- 'seqid'
   count_df$seqid <- as.character(count_df$seqid)
-
+  
+  # selecting count for specified sample_names
+  if(!is.null(sample_name)) {
+    count_df <- count_df[colnames(count_df) %in% c('seqid', 'total', sample_name)]
+    msg <- sprintf("Only returning results for samples %s", 
+                   str_c(sample_name, collapse=', '))
+    message(msg)
+  }
+  
   ## taxonomy table
   # first, separating by ;
   tax_parse <- str_split_fixed(tax[,2], ';', n=8)
@@ -84,56 +104,155 @@ oligotype_input <- function(src.folder, taxon=NULL, output=NULL){
   fasta_df <- data.frame(seqid=seqid, seq=fasta[seq_index,])
   
   # filtering for specified taxon----------------------------------------------------
-  taxon_lev_num <- length(taxon)
-  taxon_lev <- tax_name[taxon_lev_num]
+  # check if multiple taxons provided
+  logic <- is.list(taxon)
   
-  tax_sub <- tax_df %>%
-    filter(grepl(taxon[taxon_lev_num], tax_df[,taxon_lev]))
+  # when only one taxon filter provided, convert to list
+  if(!logic) taxon <- list(taxon)
   
-
-  # Retrieving sequence for taxon of interest and respective counts
-  fasta_sub <- left_join(tax_sub, fasta_df, by='seqid')
-  fasta_sub$seqid <- as.character(fasta_sub$seqid)
-  
-  count_sub <- left_join(fasta_sub, count_df, by='seqid')
-
-  # repeating sequences according to the number reads and 
-  # creating sequence headers for output fasta file----------------------------------
-  sampleid <- colnames(count_df)[!colnames(count_df) %in% c('seqid','total')]
-  
-  samp_header <- c() # headers for each sequence in a sample
-  header <- c() # headers for all samples
-  
-  samp_seq <- list() # sequences for each sequence in a sample
-  sequences <- list() # sequences for all samples
-  
-  # going through one sample at a time
-  for(i in sampleid) {
+  # when multiple taxons provided, go through one taxon at a time
+  for(k in 1:length(taxon)) {
+    taxon_lev_num <- length(taxon[[k]])
+    taxon_lev <- tax_name[taxon_lev_num]
     
-    # going through one row at a time
-    for(j in 1:nrow(count_sub)) {
-      nrep <- count_sub[j,i]
+    # returning comment on progress
+    message(sprintf("Filtering for %s", taxon[[k]][taxon_lev_num]))
+    
+    tax_sub <- tax_df %>%
+      filter(grepl(taxon[[k]][taxon_lev_num], tax_df[,taxon_lev]))
+    
+    # Retrieving sequence for taxon of interest and respective counts
+    fasta_sub <- left_join(tax_sub, fasta_df, by='seqid')
+    fasta_sub$seqid <- as.character(fasta_sub$seqid)
+    
+    count_sub <- left_join(fasta_sub, count_df, by='seqid')
+  
+    # repeating sequences according to the number reads and 
+    # creating sequence headers for output fasta file----------------------------------
+    sampleid <- colnames(count_df)[!colnames(count_df) %in% c('seqid','total')]
+  
+    samp_header <- c() # headers for each sequence in a sample
+    header <- c() # headers for all samples
+    
+    samp_seq <- list() # sequences for each sequence in a sample
+    sequences <- list() # sequences for all samples
+    
+    out <- c() # output for each sequence in sample
+    out_all <- c() # output for all samples
+    
+    # going through one sample at a time
+    for(i in sampleid) {
       
-      # repeating sequences according to number of reads
-      sequ <- count_sub[j,'seq']
-      entry <- as.list(rep(sequ, count_sub[j,i]))
+      # going through one row at a time
+      for(j in 1:nrow(count_sub)) {
+        
+        # repeating sequences according to number of reads
+        nrep <- count_sub[j,i]
+        sequ <- count_sub[j,'seq']
+        
+        # repeating the sequence the number of times that sequence is found;
+        # ignore sequence if sequence read 0 times
+        if(nrep != 0) {
+          
+          seq_rep <- rep(sequ, count_sub[j,i])
+          
+          # creating sequence headers
+          read <- str_c('Read', 1:nrep, sep='')
+          header <- str_c(i, read, sep='_')
+          
+          # adding repeated sequences and header as entry into output
+          entry_df <- data.frame(header=header, seq=seq_rep)
+          
+          out <- rbind(out, entry_df)    
+        }
+      }
       
-      samp_seq <- c(samp_seq, entry)
-
-      # creating sequence headers
-      read <- str_c('Read', 1:nrep, sep='')
-      entry <- str_c(i,read, sep='_')
+      out_all <- rbind(out_all, out)
       
-      samp_header <- c(samp_header, entry)
     }
-    sequences <- c(sequences, samp_seq)
-    header <- c(header, samp_header)
-  }
 
-  # writing file to fasta
-  if(!is.null(output)) {
-    write.fasta(sequences=sequences, names=header, file.out=output)
+    # returning sequences as list format to write as fasta
+    sequences <- as.list(out_all$seq)
+    header <- out_all$header
+    
+    # writing file to fasta
+    if(!is.null(output)) {
+      
+      # check output file name supplied for every taxon filter
+      check <- length(output) == length(taxon)
+      if(!check) {
+        stop('A filename must be provided for each fasta output', call.=FALSE)
+      }
+      else{
+        write.fasta(sequences=sequences, names=header, file.out=output[k])
+      }
+    }
+    
   }
   
+#   taxon_lev_num <- length(taxon)
+#   taxon_lev <- tax_name[taxon_lev_num]
+#   
+#   tax_sub <- tax_df %>%
+#     filter(grepl(taxon[taxon_lev_num], tax_df[,taxon_lev]))
+#   
+#   # Retrieving sequence for taxon of interest and respective counts
+#   fasta_sub <- left_join(tax_sub, fasta_df, by='seqid')
+#   fasta_sub$seqid <- as.character(fasta_sub$seqid)
+#   
+#   count_sub <- left_join(fasta_sub, count_df, by='seqid')
+#   
+#   # repeating sequences according to the number reads and 
+#   # creating sequence headers for output fasta file----------------------------------
+#   sampleid <- colnames(count_df)[!colnames(count_df) %in% c('seqid','total')]
+#   
+#   samp_header <- c() # headers for each sequence in a sample
+#   header <- c() # headers for all samples
+#   
+#   samp_seq <- list() # sequences for each sequence in a sample
+#   sequences <- list() # sequences for all samples
+# 
+#   out <- c() # output for each sequence in sample
+#   out_all <- c() # output for all samples
+#   
+#   # going through one sample at a time
+#   for(i in sampleid) {
+# 
+#     # going through one row at a time
+#     for(j in 1:nrow(count_sub)) {
+# 
+#       # repeating sequences according to number of reads
+#       nrep <- count_sub[j,i]
+#       sequ <- count_sub[j,'seq']
+#       
+#       # repeating the sequence the number of times that sequence is found;
+#       # ignore sequence if sequence read 0 times
+#       if(nrep != 0) {
+#           
+#         seq_rep <- rep(sequ, count_sub[j,i])
+#         
+#         # creating sequence headers
+#         read <- str_c('Read', 1:nrep, sep='')
+#         header <- str_c(i, read, sep='_')
+#         
+#         # adding repeated sequences and header as entry into output
+#         entry_df <- data.frame(header=header, seq=seq_rep)
+#     
+#         out <- rbind(out, entry_df)    
+#         }
+#     }
+# 
+#     out_all <- rbind(out_all, out)
+#     
+#   }
+#   
+# # returning sequences as list format to write as fasta
+# sequences <- as.list(out_all$seq)
+# header <- out_all$header
+# 
+#   # writing file to fasta
+#   if(!is.null(output)) {
+#     write.fasta(sequences=sequences, names=header, file.out=output)
+#   }
   
 }
